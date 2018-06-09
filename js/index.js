2e271441ff0e3f5305d1c498bbc5272a236efccf
laskal05 <hrh.syn.mnmk.lb@gmail.com>
2018-06-09 22:49:11 +0900

    test

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
+const locate = window.location.origin;
+let serviceWorkerPath = '/js/service-worker.js';
+
+if ( locate.indexOf('github.io') == -1 ) {
+  serviceWorkerPath = '/pwa-sample' + serviceWorkerPath;
+}
+
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/pwa-sample/js/service-worker.js')
+  navigator.serviceWorker.register(serviceWorkerPath)
     .then(function(reg) {
-      console.log('ServiceWorker registeration successed: ', registration.scope);
+      console.log('ServiceWorker registeration successed: ', reg.scope);
     }).catch(function(err) {
       console.log('ServiceWorker registration failed: ', err);
     });
